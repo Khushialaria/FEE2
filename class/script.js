15398ae0a62d909c6a27c6390b90e8cfa28b56a1
Khushialaria <khushi2122.be23@chitkara.edu.in>
2024-09-24 09:05:26 +0530

    made a new file script.js and index.html

---
+++ class/script.js
@@ -0,0 +1,69 @@
+//  global execution contex:  
+//  var a = 10 ;
+
+//  function b(){
+//     var c = 20;
+//     console.log(c);
+// }
+// b();
+// console.log(a);
+
+// const val = function sum(a , b){
+//     let c = a + b ;
+//     return c ;
+// };
+// console.log(val(5,3));
+
+// let a = 17 ;
+// console.log(a);
+// console.log(
+// const val = funstion (num){
+//     return num ** 3 ;
+// })
+
+// function printName(names,num)
+// {
+//     for(Let i = 1 ; i <=num ; i++)
+//         names();
+// }
+// Let myName = function()
+// {
+//     console.log("khushi");
+
+// }
+// names();
+
+// map method
+// let num = [ 1,2,3,4];
+// function cube(num){
+//     return num **3;
+// }
+// console.log(num.map(cube));
+
+//  TURH THE ARRAY VALUES TO UPPERCASE AND PRINT A NEW ARRAY
+
+// let arr = [ "khushi" , " lisa" , " yashvi" , "sonam"];
+// // let upperCaseArr = arr.map(function(name){return name.toUpperCase()});
+
+// // let upperCaseArr = arr.map(name => name.toUpperCase());
+
+// function capital(){
+//     return arr.toUpperCase();
+// }
+
+// console.log(arr.map(capital));
+
+// find the ven numbers
+
+// const number = [ 1 , 2 , 34 , 53 , 24 , 56 ];
+// // const evenNumbers=number.filter(num => num % 2 === 0);
+// // console.log(evenNumbers);
+
+// const envenNum = num.filter(function(num)
+// {
+//     if(num %2 == 0)
+//     {
+//         return true ;
+//     }
+//     return false ;
+// });
--- class/tempCodeRunnerFile.js
+++ class/tempCodeRunnerFile.js
@@ -1,1 +1,5 @@
-  console.log(Obj[i]);
+let num = [ 1,2,3,4];
+// function cube(num){
+//     return num **3;
+// }
+// console.log(num.map(cube));
